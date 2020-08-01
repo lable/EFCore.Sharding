74aee36e4dec56e36d364bd8fe62d026f46dc49b
y-macbook <y@y.com>
2020-08-01 15:03:21 +0800

    适配新版本

--- src/EFCore.Sharding.Tests/Startup.cs
+++ src/EFCore.Sharding.Tests/Startup.cs
@@ -22,24 +22,35 @@ namespace EFCore.Sharding.Tests
                 config.UseDatabase<ISQLiteDb2>(Config.SQLITE2, DatabaseType.SQLite);
                 config.UseDatabase<ICustomDbAccessor>(Config.CONSTRING1, DatabaseType.SqlServer);
 
-                //分表配置
-                //添加数据源
-                config.AddDataSource(Config.CONSTRING1, ReadWriteType.Read | ReadWriteType.Write, DatabaseType.SqlServer);
-                //设置分表规则
-                config.SetHashModSharding<Base_UnitTest>(nameof(Base_UnitTest.Id), 3);
+                // //分表配置
+                // //添加数据源
+                // config.AddDataSource(Config.CONSTRING1, ReadWriteType.Read | ReadWriteType.Write, DatabaseType.SqlServer);
+                // //设置分表规则
+                // config.SetHashModSharding<Base_UnitTest>(nameof(Base_UnitTest.Id), 3);
                 
 
                 // y 测试 jsonb
                 DateTime startTime = DateTime.Now.AddMinutes(-5);
                 DateTime endTime = DateTime.MaxValue;
-                config.AddAbsDb(DatabaseType.PostgreSql, absDbName: "postgres")
-                    .AddPhysicDb(ReadWriteType.Read | ReadWriteType.Write, "server=localhost;uid=postgres;password=;database=test;port=5433;commandtimeout=1024;", groupName: "postgres")
-                    .AddPhysicDbGroup(groupName: "postgres", absDbName: "postgres")
-                    .SetDateShardingRule<SqlDefaultTestModel>(nameof(SqlDefaultTestModel.ModifiedOn), absDbName: "postgres")//设置分表规则
-                    .AutoExpandByDate<SqlDefaultTestModel>(//设置为按时间自动分表
-                        ExpandByDateMode.PerMonth,
-                        (startTime, endTime, groupName: "postgres")
-                        );
+                // config.AddAbsDb(DatabaseType.PostgreSql, absDbName: "postgres")
+                //     .AddPhysicDb(ReadWriteType.Read | ReadWriteType.Write, "server=localhost;uid=postgres;password=;database=test;port=5433;commandtimeout=1024;", groupName: "postgres")
+                //     .AddPhysicDbGroup(groupName: "postgres", absDbName: "postgres")
+                //     .SetDateShardingRule<SqlDefaultTestModel>(nameof(SqlDefaultTestModel.ModifiedOn), absDbName: "postgres")//设置分表规则
+                //     .AutoExpandByDate<SqlDefaultTestModel>(//设置为按时间自动分表
+                //         ExpandByDateMode.PerMonth,
+                //         (startTime, endTime, groupName: "postgres")
+                //         );
+
+                // config.AddDataSource("server=localhost;uid=postgres;password=;database=test;port=5433;commandtimeout=1024;",
+                //     ReadWriteType.Read | ReadWriteType.Write,
+                //     DatabaseType.PostgreSql,
+                //     sourceName: "postgres"
+                //     );
+                config.AddDataSource("server=localhost;uid=postgres;password=;database=test;port=5433;commandtimeout=1024;",
+                    ReadWriteType.Read | ReadWriteType.Write,
+                    DatabaseType.PostgreSql
+                    );
+                config.SetDateSharding<SqlDefaultTestModel>(nameof(SqlDefaultTestModel.ModifiedOn), ExpandByDateMode.PerMonth, startTime);
             });
 
             ServiceProvider = services.BuildServiceProvider();
